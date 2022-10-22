fb80400eb9958245c54355716dd4abae6bbbbee0
xnatsuk <xnatsuk@gmail.com>
2022-10-22 02:01:54 -0300

    feat(ui): add footer

---
+++ components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer items-center p-4 bg-neutral text-neutral-content">
+    <div class="items-center grid-flow-col">
+      Made with
+      <a
+        class="i-simple-icons:nuxtdotjs"
+        href="https://v3.nuxtjs.org/"
+        target="_blank"
+        aria-label="go to nuxtjs website"
+      />
+      by
+      <a
+        class="hover:text-accent"
+        href="https://github.com/xnatsuk"
+        target="_blank"
+        aria-label="go to github profile"
+      >@xnatsuk</a>
+    </div>
+    <div class="items-center grid-flow-col gap-2 md:place-self-center md:justify-self-end">
+      <p> This project uses the Clarifai API.</p>
+      <a
+        class="i-carbon:logo-github"
+        href="https://github.com/xnatsuk/nuxt-smart-brain"
+        target="_blank"
+        aria-label="go to github project"
+      />
+    </div>
+  </footer>
+</template>
